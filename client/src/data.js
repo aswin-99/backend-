969ff1d31e28a9a93856b8c8988ea5aec48f3a75
unknown <achuayappan07@gmail.com>
2022-06-19 11:44:57 +0530

    my project

---
+++ client/src/data.js
@@ -0,0 +1,97 @@
+export const sliderItems = [
+    {
+      id: 1,
+      img: "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/assortment-of-colorful-ripe-tropical-fruits-top-royalty-free-image-995518546-1564092355.jpg",
+      title: "Fruits SALE",
+      desc: "DON'T COMPROMISE! GET FLAT 30% OFF.",
+      bg: "f5fafd",
+    },
+    {
+      id: 2,
+      img: "https://cdn.vox-cdn.com/thumbor/VPuqFWVsiJxxtl-wJ5i8mztTga8=/0x0:1306x734/1200x675/filters:focal(549x263:757x471)/cdn.vox-cdn.com/uploads/chorus_image/image/68490964/Best_Phone_Grid_Fall_2021_16x9.10.jpg",
+      title: "Smartphones Sale",
+      desc: "DON'T COMPROMISE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+      bg: "fcf1ed",
+    },
+    {
+      id: 3,
+      img: "https://cf.shopee.in/file/f43f927f04ea9352721bf8e6590d8386",
+      title: "Smartwatches Sale",
+      desc: "DON'T COMPROMISE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+      bg: "fbf0f4",
+    },
+  ];
+
+  export const categories = [
+    {
+      id: 1,
+      img: "https://images.news18.com/ibnlive/uploads/2022/01/fresh-fruits.jpg",
+      title: "Fruits",
+      cat:"fruit"
+    },
+    {
+      id: 2,
+      img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSaZes_Cj9TD26mUdjsTDEdeUiNMnYwdm6WQA&usqp=CAU",
+      title: "Smartphones",
+      cat:"smartphone"
+    },
+    {
+      id: 3,
+      img: "https://img.freepik.com/free-psd/modern-smartwatch-with-screen-mock-up-with-copy-space_23-2148787562.jpg?size=626&ext=jpg",
+      title: "Smartwatches",
+      cat:"smartwatch"
+    },
+    {
+      id: 4,
+      img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR-DNMNclRjIZVZ6rSHRC6qfsb4zZKMWEETtA&usqp=CAU",
+      title: "Shirt",
+      cat:"shirt"
+    },
+    {
+      id: 5,
+      img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRDqmZcWcr5JDJ5U4dcJ0ESUCOqMczzG7et_g&usqp=CAU",
+      title: "T-shirt",
+      cat:"tshirt"
+    },
+    {
+      id: 6,
+      img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTjeCIXtZlZdoOF_sCEeMbzi8-BejRkH7dvrA&usqp=CAU",
+      title: "Pants",
+      cat:"pant"
+    },
+  ];
+
+  export const popularProducts = [
+    {
+      id:1,
+      img:"https://d3o2e4jr3mxnm3.cloudfront.net/Mens-Jake-Guitar-Vintage-Crusher-Tee_68382_1_lg.png",
+    },
+    {
+      id:2,
+      img:"https://cdn.shopify.com/s/files/1/0101/4832/products/Angela_Natural_Tee.png?v=1606780388",
+    },
+    {
+      id:3,
+      img:"https://www.prada.com/content/dam/pradanux_products/U/UCS/UCS319/1YOTF010O/UCS319_1YOT_F010O_S_182_SLF.png",
+    },
+    {
+      id:4,
+      img:"https://www.burdastyle.com/pub/media/catalog/product/cache/7bd3727382ce0a860b68816435d76e26/107/BUS-PAT-BURTE-1320516/1170x1470_BS_2016_05_132_front.png",
+    },
+    {
+      id:5,
+      img:"https://images.ctfassets.net/5gvckmvm9289/3BlDoZxSSjqAvv1jBJP7TH/65f9a95484117730ace42abf64e89572/Noissue-x-Creatsy-Tote-Bag-Mockup-Bundle-_4_-2.png",
+    },
+    {
+      id:6,
+      img:"https://d3o2e4jr3mxnm3.cloudfront.net/Rocket-Vintage-Chill-Cap_66374_1_lg.png",
+    },
+    {
+      id:7,
+      img:"https://www.vintageindustries.nl/download_front/qympzk1762/2217_Arrow_Jacket_Forest.png",
+    },
+    {
+      id:8,
+      img:"https://www.pngarts.com/files/3/Women-Jacket-PNG-High-Quality-Image.png",
+    },
+  ]
